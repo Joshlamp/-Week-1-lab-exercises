2297cef09e145b6b6580170fd22a108a43388c6c
joshlam <joshualamprecht142@gmail.com>
2025-10-07 15:42:53 +0100

    Add files via upload

---
+++ week1_lab.py
@@ -0,0 +1,8 @@
+print("hello world ")
+student_name=("Josh")
+student_age=18
+print ("The student name is ",student_name,"and the age is ",student_age)
+num=6 
+num2=7
+total=num+num2
+print("The total is ",total)
